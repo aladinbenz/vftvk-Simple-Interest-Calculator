b86729bc751bdd6c7609ee2a7426ed9d38ac19bf
aladinbenz <56277363+aladinbenz@users.noreply.github.com>
2021-12-02 11:05:02 +0100

    updated calculator according to requirements

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
 }
-        
+
+function compute() {
+  // get needed variables
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+
+  // calculattions
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    // update result
+    var result = (document.getElementById("result").innerHTML =
+      "If you deposit <b>" +
+      principal +
+      "</b>, </br> at an interest rate of <b>" +
+      rate +
+      "</b></br>You will receive an amount of <b>" +
+      interest +
+      "</b>,</br>in the year <b>" +
+      year +
+      "</b></p>");
+  }
+}
